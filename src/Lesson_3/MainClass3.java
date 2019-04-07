e74c40980aaba9d8ef3fec4dd27a12d051175033
iiahmetov <iiahmetov@gmail.com>
2019-04-07 13:49:16 +0300

    Решил 2 задачу

--- src/Lesson_3/MainClass3.java
+++ src/Lesson_3/MainClass3.java
@@ -7,7 +7,7 @@ public class MainClass3 {
     public static void main(String[] args) {
         String [] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
         ugadajka();
-
+        polechudes(words);
     }
 
     public static void ugadajka() {
@@ -43,6 +43,33 @@ public class MainClass3 {
         sc.close();                                                         //закрываю сканер
     }
 
-//    public static void
+    public static void polechudes(String[] arr) {
+        Random rand = new Random();                                                                     //создаю объект генератора случайных чисел
+        Scanner sc = new Scanner(System.in);                                                            //создаю объект сканер
+        boolean flag = false;                                                                           //флаг выхода из цикла
+        System.out.println("Отгадайте загаданное слово.");
+        int p = 0;                                                                                      //счётчик попыток
+        int x = rand.nextInt(arr.length);                                                               //случайный выбор слова из массива слов
+//        char [] bukv = {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'};     //создаю массив символов для разгадывания слова по буквам с сохраненем букв от прердыдущихх попыток
+        do {
+            System.out.println("Попытка " + (p+1) + ".");                                               //вывожу номер попытки
+            char [] bukv = {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}; //создаю массив символов для разгадывания слова по буквам без сохранения результатов предыдущей попытки
+            String ans = sc.nextLine();                                                                 //считываю вариант ответа из консоли
+            if (ans.equals(arr[x])) {                                                                   //проверяю угадал ли игрок слово
+                System.out.println("Вы угадали!");
+                flag = true;                                                                            //в случае успеха создаю условия для выхода из цикла (использовать для этого break менее "красивое" решение или нормально?)
+            } else {
+                System.out.println("Не угадали, но давайте посмотрим по буквам!");                      //в случае если игрок не угадал вывожу соответствующее сообщение
+                for (int i = 0; i < ans.length() && i < arr[x].length(); i++) {                         //создаю цикл с количеством итераций равным длине минимального из двух слов(загаданного/названного игроком)
+                    if (ans.charAt(i) == arr[x].charAt(i)) {                                            //сравниваю посимвольно ответ игрока с загаданным словом
+                        bukv[i] = arr[x].charAt(i);                                                     //в случае успеха "раскрываю" букву в массиве символов
+                    }
+                }
+                System.out.println(bukv);                                                               //вывожу разгаданные буквы
+                }
+            p++;                                                                                        //увеличиваю счётчик попыток на 1
+        } while (flag == false);
+        sc.close();                                                                                     //закрываю объект сканер
+    }
 }
 
