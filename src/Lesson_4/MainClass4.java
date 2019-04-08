93f5bd8bf8a9fcda16d947f33eac7c139f80b7ce
iiahmetov <iiahmetov@gmail.com>
2019-04-08 21:25:23 +0300

    Новая ветка для 4 дз

---
+++ src/Lesson_4/MainClass4.java
@@ -0,0 +1,15 @@
+package Lesson_4;
+
+import java.security.PublicKey;
+import java.util.Random;
+import java.util.Scanner;
+
+public class MainClass4 {
+    public static void main(String[] args) {
+
+    }
+
+    public static Random rand = new Random();
+    public static Scanner scan = new Scanner(System.in);
+    public static 
+}
